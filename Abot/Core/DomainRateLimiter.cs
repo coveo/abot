28773edbef5c359bf96fb508ad33b81201232388
Nicolas Rodrigues <nrodrigues@coveo.com>
2017-07-17 15:51:40 -0400

    Modified DomainRateLimiter to be able to override GetRateLimter and to be able to use _rateLimiterLookup in a derived class

--- Abot/Core/DomainRateLimiter.cs
+++ Abot/Core/DomainRateLimiter.cs
@@ -36,7 +36,7 @@ namespace Abot.Core
     public class DomainRateLimiter : IDomainRateLimiter
     {
         static ILog _logger = LogManager.GetLogger("AbotLogger");
-        ConcurrentDictionary<string, IRateLimiter> _rateLimiterLookup = new ConcurrentDictionary<string, IRateLimiter>();
+        protected internal ConcurrentDictionary<string, IRateLimiter> _rateLimiterLookup = new ConcurrentDictionary<string, IRateLimiter>();
         long _defaultMinCrawlDelayInMillisecs;
 
         public DomainRateLimiter(long minCrawlDelayMillisecs)
@@ -101,7 +101,7 @@ namespace Abot.Core
         }
 
 
-        private IRateLimiter GetRateLimter(Uri uri, long minCrawlDelayInMillisecs)
+        protected virtual IRateLimiter GetRateLimter(Uri uri, long minCrawlDelayInMillisecs)
         {
             IRateLimiter rateLimiter;
             _rateLimiterLookup.TryGetValue(uri.Authority, out rateLimiter);
